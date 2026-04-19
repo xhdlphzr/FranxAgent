62eff51ecba79b54021e23abbb98f048fd5a95b0
xhdlphzr <xhdlphzr@163.com>
2026-04-19 10:16:14 +0800

    copyleft

--- similarity/tool.py
+++ similarity/tool.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2026 xhdlphzr, zhiziwj
+# Copyright (C) 2026 zhiziwj, xhdlphzr
 # This file is part of FranxAgent.
 # FranxAgent is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
 # FranxAgent is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
